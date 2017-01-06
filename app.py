c71d4fc17af1f397bfdc54d7c9899e0dcdc54797
tscrip <https://twitter.com/thetscrip>
2017-01-06 13:47:28 -0600

    Updated to joke API

--- app.py
+++ app.py
@@ -29,66 +29,36 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
+    if req.get("result").get("action") != "chuckNorrisJoke":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
-    result = urllib.urlopen(yql_url).read()
+    baseurl = "http://api.icndb.com/jokes/random?exclude=[nerdy,explicit]"
+    result = urllib.urlopen(baseurl).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
     return res
 
-
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
-
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-
-
 def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
+    success = data.get('success')
+    if not success:
         return {}
+    else:
+        value = data.get('value')
+        if value is None:
+            return {}
+        else:
+            joke = value.get('joke')
 
-    result = query.get('results')
-    if result is None:
-        return {}
 
-    channel = result.get('channel')
-    if channel is None:
-        return {}
 
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
-    print("Response:")
-    print(speech)
+    print("Joke:")
+    print(joke)
 
     return {
-        "speech": speech,
-        "displayText": speech,
+        "speech": joke,
+        "displayText": joke,
         # "data": data,
         # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
+        "source": "apiai-chuck-norris-webhook"
     }
 
 
